44edb14830b4d46057f1567bdce12b2b098a5e37
Vlad Sliusar <31670686+vlads25@users.noreply.github.com>
2018-05-08 12:14:31 -0700

    If applied, this commit will fix minor bugs
    Text and container allignment for mobile, contact form error

--- mail.php
+++ mail.php
@@ -1,10 +1,17 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['subject'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "sliusar@wisc.edu";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank you! Your message was sent.";
+<?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['subject'];
+    
+    $email_from = "sliusar@wisc.edu";
+        
+    $formcontent="From: $name \n Message: $message";
+    $subject = "Contact Form";
+
+    $recipient = "sliusar@wisc.edu";
+ 
+    $mailheader = "From: $email \r\n"; 
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    header("location: contact.html");
+    echo "Thank you! Your message was sent.";
 ?>
